190d5105e77404518fe6a226510d03bc261c863d
John Holland <john.hollandr@gmai.com>
2018-04-22 21:02:57 -0500

    syntax

--- python/gamePlayer.py
+++ python/gamePlayer.py
@@ -7,7 +7,7 @@ from messages import *
 ADDRESS = os.environ.get("BIND_ADDRESS","localhost")
 PORT = os.environ.get("BIND_PORT", 11293)
 
-FREQUENCY = 2
+FREQUENCY = 30
 
 class GamePlayer(rm.ProtoModule):
     def __init__(self, addr, port):
