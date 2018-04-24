9795c2e99b396d189c6d7a40586cea49e24617ea
John Holland <john.hollandr@gmai.com>
2018-04-23 23:08:53 -0500

    debug

--- python/gamePlayer.py
+++ python/gamePlayer.py
@@ -36,9 +36,9 @@ class GamePlayer(rm.ProtoModule):
         if self.distance:
             print("\n" * 30)
             # print("Front Center: ", self.distance.front_center)
-            print("Front Left: ", self.distance.front_left) # GOOD
+            # print("Front Left: ", self.distance.front_left) # GOOD
             # print("Front Right: ", self.distance.front_right) # BAD!!!
-            # print("Rear Left: ", self.distance.rear_left) #GOOD
+            print("Rear Left: ", self.distance.rear_left) #GOOD
             # print("Rear Right: ", self.distance.rear_right) #BAD!!!
         # if self.moving:
         #     # msg.velocity = 98
