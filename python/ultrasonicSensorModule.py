452882eaa16dc94425cd47c672500d8808e9cd92
John Holland <john.hollandr@gmai.com>
2018-04-23 21:43:31 -0500

    debug

--- python/ultrasonicSensorModule.py
+++ python/ultrasonicSensorModule.py
@@ -48,9 +48,9 @@ class UltrasonicSensorModule(rm.ProtoModule):
         # msg.rear_left = self.pulse(REAR_LFT)
         msg.front_center = 0#self.pulse(FRT_CTR)
         msg.front_left = self.pulse(FRT_LFT)
-        msg.front_right = 0#self.pulse(FRT_RGT)
-        msg.rear_left = self.pulse(REAR_LFT)
-        msg.rear_right = self.pulse(REAR_RGT)
+        msg.front_right = self.pulse(FRT_RGT)
+        msg.rear_left = 0#self.pulse(REAR_LFT)
+        msg.rear_right = 0#self.pulse(REAR_RGT)
 
         msg = msg.SerializeToString()
         self.write(msg, MsgType.ULTRASONIC_ARRAY)
