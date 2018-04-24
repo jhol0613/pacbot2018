7a990f7f11472490c4225f11728de8870c97b88b
John Holland <john.hollandr@gmai.com>
2018-04-24 11:49:20 -0500

    debug

--- python/test_code/motor_test.py
+++ python/test_code/motor_test.py
@@ -25,7 +25,7 @@ def setup():
 	IO.output(CTRL_2, False)
 
 	IO.setup(ENCODER, IO.IN)
-	IO.add_event_detect(ENCODER, IO.BOTH, callback=encoderCallback)
+	IO.add_event_detect(ENCODER, IO.BOTH, self.encoderCallback)
 
 	IO.setup(PWM_PIN, IO.OUT)
 	global PWM
