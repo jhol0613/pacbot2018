079f47548be4d172ec213640c581312cf0ed0ece
John Holland <john.hollandr@gmai.com>
2018-04-25 11:48:45 -0500

    p tuning

--- python/p_tuning.m
+++ python/p_tuning.m
@@ -1,9 +1,11 @@
+clf
 data = csvread('p_test.csv');
 t = linspace(1, 100, length(data));
 hold on
 for i = 1:4
     plot(t, data(:,i));
 end
+plot(t, 7*ones(1, length(t)), '--');
 legend('front left', 'front right', 'rear left', 'rear right')
 xlabel('time (arbitrary units)')
 ylabel('sensor reading (cm)')
