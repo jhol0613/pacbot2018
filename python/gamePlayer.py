ae87abd0ede24cc6a107b1ae2245589a801202e5
John Holland <john.hollandr@gmai.com>
2018-04-25 17:54:27 -0500

    tuning

--- python/gamePlayer.py
+++ python/gamePlayer.py
@@ -17,8 +17,8 @@ ACTION_SEQUENCE_FILES = ["paths/testPath.txt"]
 # Constants for rotations
 ROTATIONAL_CORRECTION_CONSTANT = 1 # adjustment factor for unequal turning
 ROTATIONAL_SPEED = 40 # speed at which rotations occur
-ODOMETRY_LEFT_TURN_THRESHOLD = 240 # odometer cutoff for finishing left turn
-ODOMETRY_RIGHT_TURN_THRESHOLD = 240 # odometer cutoff for finishing right turn
+ODOMETRY_LEFT_TURN_THRESHOLD = 220 # odometer cutoff for finishing left turn
+ODOMETRY_RIGHT_TURN_THRESHOLD = 220 # odometer cutoff for finishing right turn
 
 # Constants for pause
 PAUSE_TIME = 0.5 # length of a typical pause
