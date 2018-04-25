e7ee9b07d64397751711331bc0a0c5e743dbe545
John Holland <john.hollandr@gmai.com>
2018-04-25 16:55:53 -0500

    tuning

--- python/gamePlayer.py
+++ python/gamePlayer.py
@@ -30,7 +30,7 @@ FRONT_SENSOR_THRESHOLD = 8.0 # minimum sensor value before stopping forward moti
 SENSOR_CASE_THRESHOLD = 11 # max sensor reading that is considered when centering path
 SENSOR_CASE_MIN = 3.5 # assume sensor readings below this are garbage and don't consider
 SENSOR_TARGET = 7.0 # target value that sensors try to return to
-P_MULTIPLIER = 3.0 # this is multiplied by calculated position correction factor to determine omega
+P_MULTIPLIER = 5.0 # this is multiplied by calculated position correction factor to determine omega
 D_MULTIPLIER = 6.0 # this is multiplied by calculated direction correction factor to determine omega
 
 class GamePlayer(rm.ProtoModule):
