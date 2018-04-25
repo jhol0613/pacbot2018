87436aac479a67961f5bb990742e3b71820299a7
John Holland <john.hollandr@gmai.com>
2018-04-25 14:00:46 -0500

    p tuninggit

--- python/p_tuning.m
+++ python/p_tuning.m
@@ -3,13 +3,15 @@ data = csvread('p_test.csv');
 %data = data(1:20, :);
 t = linspace(1, 100, length(data));
 hold on
-for i = 1:5
-    plot(t, data(:,i));
-end
-plot(t, 7*ones(1, length(t)), '--');
-plot(t, zeros(1, length(t)), '--');
-legend('front left', 'front right', 'rear left', 'rear right', ...
-    'correction factor')
-xlabel('time (arbitrary units)')
-ylabel('sensor reading (cm)')
+subplot(2, 1, 1)
+plot(t, data(:,1), t, data(:,2), t, data(:,3), t, data(:,4), ...
+    t, 7*ones(1, length(t)), '--' );
+
+legend('front left', 'front right', 'rear left', 'rear right');
+xlabel('time (arbitrary units)');
+ylabel('sensor reading (cm)');
+
+subplot(2, 1, 2);
+plot(t, data(:,5), t, zeros(1, length(t)), '--');
+
 shg
